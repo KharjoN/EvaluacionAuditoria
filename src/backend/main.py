9348894ddb6e1230e8a1e71ce16adbf8327f26a4
KharjoN <darkyloca@hotmail.com>
2025-07-18 18:47:56 -0400

    Version 3(Arreglo de carga de html y errores menores)

--- src/backend/main.py
+++ src/backend/main.py
@@ -103,7 +103,8 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:5500",  
-    "http://127.0.0.1:5500", 
+    "http://127.0.0.1:5500",
+    "http://localhost:8080", 
     "null",
 ]
 
